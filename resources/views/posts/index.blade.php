79a16f48310b5b458eef5d1b35af8458d450f3d1
sergio <sermo14.sm@gmail.com>
2021-05-12 20:23:45 +0200

    prueba

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -15,7 +15,7 @@
       
 
             <div class="col-12 btn-group mb-3">
-                <button type="button" class="btn btn-block dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <button type="button" class="btn btn-block dropdown-toggle text-left" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Action
                 </button>
 
