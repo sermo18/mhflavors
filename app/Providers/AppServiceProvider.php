c84ddf581aa0a21ae59b883327b851c26cdad811
sergio <sermo14.sm@gmail.com>
2021-05-12 14:06:08 +0200

    add Procfileç

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,26 +6,38 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
+
+
+
+
     /**
      * Register any application services.
      *
      * @return void
      */
     public function register()
-    {
-        //
-    }
+{
+   if (env('REDIRECT_HTTPS')) {
+       $this->app['request']->server->set('HTTPS', true);
+   }
+}
 
     /**
      * Bootstrap any application services.
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
+
+    if (env('REDIRECT_HTTPS')) {
+        $url->formatScheme('https://');
+     }
+
         Route::resourceVerbs([
             'create' => 'crear',
             'edit' => 'editar'
@@ -34,4 +46,5 @@ class AppServiceProvider extends ServiceProvider
         Paginator::useBootstrap();
 
     }
+
 }
