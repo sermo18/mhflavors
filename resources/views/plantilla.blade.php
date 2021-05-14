ac5419b59a6a0c856f2905b4436689e6e02a5621
sergio <sermo14.sm@gmail.com>
2021-05-14 17:31:28 +0200

    estilos

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -8,13 +8,6 @@
 
  <body>
  @include('partials.nav')
-<div class="row justify-content-end align-content-end" style="width: 100%; height: 60%; ">
-    <div class="col-12  text-center "><h1 class="text-light" style="font-size: 90px;">MHFlavors
-    </h1></div>
-    <h4 class="text-light col-12 text-center" >" Descubre Saborea, Vive. "</h4>
-</div>
-</div>
-
 
 @yield('contenido')
 
