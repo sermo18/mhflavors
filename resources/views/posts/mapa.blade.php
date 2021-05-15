eecb572898d7340c6e41c0413b8bd899f12fbe07
sergio <sermo14.sm@gmail.com>
2021-05-15 09:58:56 +0200

    estilos

--- resources/views/posts/mapa.blade.php
+++ resources/views/posts/mapa.blade.php
@@ -6,8 +6,9 @@
 <div class="row m-4">
             <h1>Principales Lounges</h1>
         </div>
-    
-    <iframe class="mb-4" src="https://www.google.com/maps/d/embed?mid=1LmlfBYmV4ljXxjqELdmLOBscH02e-GxV&hl=es" width="100%" height="100%"></iframe>
+        <div id="map-container" class="z-depth-1-half map-container">
 
+    <iframe class="mb-4" src="https://www.google.com/maps/d/embed?mid=1LmlfBYmV4ljXxjqELdmLOBscH02e-GxV&hl=es" width="100%" height="100%"></iframe>
+        </div>
 </div>
 @endsection
