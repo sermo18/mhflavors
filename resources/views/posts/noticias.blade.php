d9741c2c97eb040988f181a951a895bff53542bb
sergio <sermo14.sm@gmail.com>
2021-05-15 12:02:29 +0200

    estilos

--- resources/views/posts/noticias.blade.php
+++ resources/views/posts/noticias.blade.php
@@ -13,9 +13,9 @@
                     <h5 class="card-title">Las cachimbas, la moda de un año de pandemia
 </h5>
                     <p class="card-text">Ahora los fumadores de cachimbas desean tener en su propio hogar cachimbas, accesorios y tabacos para fumar sin salir de casa y pasar mucho mejor el encierro por la pandemia...</p>
-                    <a href="https://www.diariodecadiz.es/cadiz/cachimbas-moda-ano-pandemia_0_1573642960.html" class="btn btn-dark">Saber Más</a>
                     <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    
+                    <a href="https://www.diariodecadiz.es/cadiz/cachimbas-moda-ano-pandemia_0_1573642960.html" class="btn btn-dark m-2">Saber Más</a>
+
 
                 </div>
             </div>
@@ -35,9 +35,9 @@
                                     
                                     <p class="card-text">Se está incrementando el número de fumadores...</p>
                                     
-                                    <a href="https://www.madridiario.es/492107/las-cachimbas-una-moda-incipiente" class="btn btn-dark">Saber Más</a>
 
                                     <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    <a href="https://www.madridiario.es/492107/las-cachimbas-una-moda-incipiente" class="btn btn-dark m-2">Saber Más</a>
 
                                 </div>
                             </div>
@@ -54,10 +54,10 @@
                                 <div class="card-body">
                                     <h5 class="card-title">El negocio de la cachimba florece en España al borde de la ley</h5>
                                     <p class="card-text">A falta de cerrar los datos de diciembre...</p>
-                                                              <a href="https://www.eldiario.es/economia/negocio-cachimba-florece-espana-borde-ley_1_6820154.html" class="btn btn-dark">Saber Más</a>
 
                                     <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                             
+                                    <a href="https://www.eldiario.es/economia/negocio-cachimba-florece-espana-borde-ley_1_6820154.html" class="btn btn-dark m-2">Saber Más</a>
 
                                 </div>
                             </div>
@@ -74,8 +74,9 @@
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <a href="#" class="btn btn-dark">Saber Más</a>
+                    
                     <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <a href="#" class="btn btn-dark m-2">Saber Más</a>
 
 
                 </div>
@@ -87,9 +88,10 @@
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <a href="#" class="btn btn-dark">Saber Más</a>
+                    
                     <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
 
+                    <a href="#" class="btn btn-dark m-2">Saber Más</a>
 
                 </div>
             </div>
@@ -100,9 +102,10 @@
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <a href="#" class="btn btn-dark">Saber Más</a>
+                
                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
 
+                 <a href="#" class="btn btn-dark m-2">Saber Más</a>
 
                 </div>
             </div>
@@ -113,7 +116,6 @@
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <a href="#" class="btn btn-dark">Saber Más</a>
                     <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
 
 
