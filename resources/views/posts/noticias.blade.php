b87786b35a00be0db10794bd26a6f025a84ea682
sergio <sermo14.sm@gmail.com>
2021-05-15 11:18:09 +0200

    estilos

--- resources/views/posts/noticias.blade.php
+++ resources/views/posts/noticias.blade.php
@@ -6,7 +6,7 @@
         <h1>Últimas Noticias</h1>
     </div>
     <div class="row">
-        <div class="col-12 col-md-6">
+        <div class="col-12 col-md-4">
             <div class="card mb-3">
                 <img src="https://www.diariodecadiz.es/2021/05/14/cadiz/cachimbas-moda-ano-pandemia_1573952634_138491982_667x375.jpg" class="card-img" alt="...">
                 <div class="card-body">
@@ -19,19 +19,19 @@
                 </div>
             </div>
         </div>
-        <div class="col-12 col-md-6">
+        <div class="col-12 col-md-8">
             <div class="row">
                 <div class="col-12">
                     <div class="card mb-3 ">
                         <div class="row no-gutters">
-                            <div class="col-md-4">
+                            <div class="col-md-6">
                                 <img src="https://www.madridiario.es/fotos/1/231545_unnamed-71_thumb_722.jpg" class="card-img" alt="...">
                             </div>
-                            <div class="col-md-8">
+                            <div class="col-md-6">
                                 <div class="card-body">
                                     <h5 class="card-title">Las cachimbas, una moda incipiente
 </h5>
-                                    <p class="card-text">Desde hace un par de años, se está incrementando el número de fumadores que utilizan la cachimba como sistema alternativo al cigarrillo, pipa o puro...</p>
+                                    <p class="card-text">Se está incrementando el número de fumadores que utilizan la cachimba como sistema alternativo al cigarrillo, pipa o puro...</p>
                                     <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                                     https://www.madridiario.es/492107/las-cachimbas-una-moda-incipiente
                                     <a href="https://www.diariodecadiz.es/cadiz/cachimbas-moda-ano-pandemia_0_1573642960.html">Noticia importante</a>
@@ -43,13 +43,13 @@
                 <div class="col-12 ">
                     <div class="card mb-3">
                         <div class="row no-gutters">
-                            <div class="col-md-4">
+                            <div class="col-md-6">
                                 <img src="https://static.eldiario.es/clip/d960ea2b-1cd0-4930-ba58-9d6a2a7be296_16-9-aspect-ratio_default_0.jpg" class="card-img" alt="...">
                             </div>
-                            <div class="col-md-8">
+                            <div class="col-md-6">
                                 <div class="card-body">
                                     <h5 class="card-title">El negocio de la cachimba florece en España al borde de la ley</h5>
-                                    <p class="card-text">A falta de cerrar los datos de diciembre, en 2020 se comercializaron más de 2 millones de kilos de tabaco de pipa de agua, lejos de los 688.000 kilos de 2016, y el negocio supera ya los 200 millones de euros, frente a los 50 de entonces...</p>
+                                    <p class="card-text">A falta de cerrar los datos de diciembre, en 2020 se comercializaron más de 2 millones de kilos de tabaco de pipa de agua...</p>
                                     <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                                     https://www.eldiario.es/economia/negocio-cachimba-florece-espana-borde-ley_1_6820154.html
                                 </div>
