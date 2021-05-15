06038c19e1fdc1e0b5cb2ea8512ba4e7bb19be97
sergio <sermo14.sm@gmail.com>
2021-05-15 10:22:16 +0200

    primer commit local

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -29,8 +29,8 @@
         @forelse($sabores as $sabor)
 
 
-        <div class="col-12 col-sm-6 col-md-4 mb-3 quitar shadow p-3">
-            <div class="card text-left ponerImagenes">
+        <div class="col-12 col-sm-6 col-md-4 mb-3 quitar ">
+            <div class="card text-left ponerImagenes shadow p-3">
                 <div class="card-body text-white " style="height: 400px; ">
                     <h3 class="card-title text-left" style="height: 70px;">{{ $sabor->nombre }}</h3>
                     <h5 class="card-text p-3">Descripcion: {{$sabor->descripcion }}</h5>
