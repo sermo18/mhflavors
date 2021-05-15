b723eecad5fa78b25177c2b1f8f8a13eb259280e
sergio <sermo14.sm@gmail.com>
2021-05-15 11:48:33 +0200

    estilos

--- resources/views/posts/noticias.blade.php
+++ resources/views/posts/noticias.blade.php
@@ -27,7 +27,7 @@
                     <div class="card ">
                         <div class="row no-gutters">
                             <div class="col-md-7">
-                                <img src="https://www.madridiario.es/fotos/1/231545_unnamed-71_thumb_722.jpg" class="card-img" alt="...">
+                                <img src="https://www.madridiario.es/fotos/1/231545_unnamed-71_thumb_722.jpg" style="height: 200px;" class="card-img" alt="...">
                             </div>
                             <div class="col-md-5">
                                 <div class="card-body">
@@ -48,7 +48,7 @@
                     <div class="card ">
                         <div class="row no-gutters">
                             <div class="col-md-7">
-                                <img src="https://static.eldiario.es/clip/d960ea2b-1cd0-4930-ba58-9d6a2a7be296_16-9-aspect-ratio_default_0.jpg" class="card-img" alt="...">
+                                <img src="https://static.eldiario.es/clip/d960ea2b-1cd0-4930-ba58-9d6a2a7be296_16-9-aspect-ratio_default_0.jpg" style="height: 200px;" class="card-img" alt="...">
                             </div>
                             <div class="col-md-5">
                                 <div class="card-body">
@@ -70,7 +70,7 @@
     <div class="row mt-4">
         <div class="col-12 col-md-3">
             <div class="card mb-3">
-                <img src="https://21noticias.com/wp-content/uploads/2021/02/91e4ccec5e1a902ba74fd6c66373a852-400x280.jpg"  style="height: 100px;" class="card-img" alt="...">
+                <img src="https://21noticias.com/wp-content/uploads/2021/02/91e4ccec5e1a902ba74fd6c66373a852-400x280.jpg"  style="height: 200px;" class="card-img" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -82,7 +82,7 @@
         </div>
         <div class="col-12 col-md-3">
             <div class="card mb-3">
-                <img src="https://879669.smushcdn.com/1707657/wp-content/uploads/2021/03/oficios-cachimba.jpg?lossy=1&strip=1&webp=1" style="height: 100px;" class="card-img" alt="...">
+                <img src="https://879669.smushcdn.com/1707657/wp-content/uploads/2021/03/oficios-cachimba.jpg?lossy=1&strip=1&webp=1" style="height: 200px;" class="card-img" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -94,7 +94,7 @@
         </div>
         <div class="col-12 col-md-3">
             <div class="card mb-3">
-                <img src="https://879669.smushcdn.com/1707657/wp-content/uploads/2020/03/como-limpiar-shisha.jpg?lossy=1&strip=1&webp=1" class="card-img" style="height: 100px;" alt="...">
+                <img src="https://879669.smushcdn.com/1707657/wp-content/uploads/2020/03/como-limpiar-shisha.jpg?lossy=1&strip=1&webp=1" class="card-img" style="height: 200px;" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -106,7 +106,7 @@
         </div>
         <div class="col-12 col-md-3">
             <div class="card mb-3">
-                <img src="https://estaticos-cdn.prensaiberica.es/clip/09190588-d4d0-49ac-a40e-bbfbab69a6fb_16-9-aspect-ratio_default_0.jpg" class="card-img" style="height: 100px;" alt="...">
+                <img src="https://estaticos-cdn.prensaiberica.es/clip/09190588-d4d0-49ac-a40e-bbfbab69a6fb_16-9-aspect-ratio_default_0.jpg" class="card-img" style="height: 200px;" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
