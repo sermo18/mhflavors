87d7d7ff124b62621a778e9490b099881eb2b565
sergio <sermo14.sm@gmail.com>
2021-05-15 10:09:36 +0200

    primer commit local

--- resources/views/posts/mapa.blade.php
+++ resources/views/posts/mapa.blade.php
@@ -8,8 +8,8 @@
         </div>
         <div id="map-container" class=".z-depth-3 map-container" style="height: 100%">
 
-    <iframe class="mb-4" src="https://www.google.com/maps/d/embed?mid=1LmlfBYmV4ljXxjqELdmLOBscH02e-GxV&hl=es" frameborder="0"
-    style="border:0" allowfullscreen></iframe>
+    <iframe class="mb-4" src="https://www.google.com/maps/d/embed?mid=1LmlfBYmV4ljXxjqELdmLOBscH02e-GxV&hl=es" width="100%" height="100%" frameborder="0"
+    style="border:0" ></iframe>
         </div>
 </div>
 @endsection
