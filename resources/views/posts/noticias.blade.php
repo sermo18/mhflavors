edf77223eaa4c649edc87a4bb296adf439e16b36
sergio <sermo14.sm@gmail.com>
2021-05-15 10:59:33 +0200

    estilos

--- resources/views/posts/noticias.blade.php
+++ resources/views/posts/noticias.blade.php
@@ -8,9 +8,7 @@
     <div class="row">
         <div class="col-6">
             <div class="card mb-3">
-
                 <img src="..." class="card-img" alt="...">
-
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -20,7 +18,7 @@
             </div>
         </div>
     </div>
-    <div class="col-6">
+    <div class="col-5">
         <div class="row">
             <div class="col-12">
                 <div class="card mb-3 ">
