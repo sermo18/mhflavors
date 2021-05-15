eaa99b6b848b97edf72f45d79e953d30f1d360af
sergio <sermo14.sm@gmail.com>
2021-05-15 10:42:33 +0200

    estilos

--- resources/views/posts/contacto.blade.php
+++ resources/views/posts/contacto.blade.php
@@ -24,13 +24,13 @@
         <div class="form-group row">
             <label for="exampleFormControlFile1" class="col-sm-2 col-form-label">Archivo adjunto</label>
             <div class="col-sm-10">
-                <input type="file" class="form-control-file shadow p-3" id="exampleFormControlFile1">
+                <input type="file" class="form-control-file" id="exampleFormControlFile1">
             </div>
         </div>
         <div class="form-group row">
             <label for="exampleFormControlTextarea1" class="col-sm-2 col-form-label">Example textarea</label>
             <div class="col-sm-10">
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Escribe aqui tu mensaje"></textarea>
+                <textarea class="form-control shadow p-3" id="exampleFormControlTextarea1" rows="3" placeholder="Escribe aqui tu mensaje"></textarea>
 
             </div>
         </div>
