6cfc0f4d57c24b2e1c91541ee60893fe266f0f2d
sergio <sermo14.sm@gmail.com>
2021-05-15 11:15:27 +0200

    estilos

--- resources/views/posts/noticias.blade.php
+++ resources/views/posts/noticias.blade.php
@@ -8,10 +8,11 @@
     <div class="row">
         <div class="col-12 col-md-6">
             <div class="card mb-3">
-                <img src="..." class="card-img" alt="...">
+                <img src="https://www.diariodecadiz.es/2021/05/14/cadiz/cachimbas-moda-ano-pandemia_1573952634_138491982_667x375.jpg" class="card-img" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <h5 class="card-title">Las cachimbas, la moda de un año de pandemia
+</h5>
+                    <p class="card-text">Ahora los fumadores de cachimbas desean tener en su propio hogar cachimbas, accesorios y tabacos para fumar sin salir de casa y pasar mucho mejor el encierro por la pandemia...</p>
                     <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
 
 
@@ -24,13 +25,15 @@
                     <div class="card mb-3 ">
                         <div class="row no-gutters">
                             <div class="col-md-4">
-                                <img src="..." class="card-img" alt="...">
+                                <img src="https://www.madridiario.es/fotos/1/231545_unnamed-71_thumb_722.jpg" class="card-img" alt="...">
                             </div>
                             <div class="col-md-8">
                                 <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <h5 class="card-title">Las cachimbas, una moda incipiente
+</h5>
+                                    <p class="card-text">Desde hace un par de años, se está incrementando el número de fumadores que utilizan la cachimba como sistema alternativo al cigarrillo, pipa o puro...</p>
                                     <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    https://www.madridiario.es/492107/las-cachimbas-una-moda-incipiente
                                     <a href="https://www.diariodecadiz.es/cadiz/cachimbas-moda-ano-pandemia_0_1573642960.html">Noticia importante</a>
                                 </div>
                             </div>
@@ -41,13 +44,14 @@
                     <div class="card mb-3">
                         <div class="row no-gutters">
                             <div class="col-md-4">
-                                <img src="..." class="card-img" alt="...">
+                                <img src="https://static.eldiario.es/clip/d960ea2b-1cd0-4930-ba58-9d6a2a7be296_16-9-aspect-ratio_default_0.jpg" class="card-img" alt="...">
                             </div>
                             <div class="col-md-8">
                                 <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <h5 class="card-title">El negocio de la cachimba florece en España al borde de la ley</h5>
+                                    <p class="card-text">A falta de cerrar los datos de diciembre, en 2020 se comercializaron más de 2 millones de kilos de tabaco de pipa de agua, lejos de los 688.000 kilos de 2016, y el negocio supera ya los 200 millones de euros, frente a los 50 de entonces...</p>
                                     <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    https://www.eldiario.es/economia/negocio-cachimba-florece-espana-borde-ley_1_6820154.html
                                 </div>
                             </div>
                         </div>
