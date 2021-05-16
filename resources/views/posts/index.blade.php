4e989dc41a286259fb0714839238f440bfdf29d9
sergio <sermo14.sm@gmail.com>
2021-05-16 11:16:43 +0200

    primer commit local

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -40,7 +40,15 @@
                     @else
                     <h5 class="card-text p-3">Valoración: {{ $sabor->valoracion }} / 5 </h5>
                     @endif
+                    @if(strlen($sabor->descripcion) >19)
+                    
+                    @elseif(strlen($sabor->descripcion) >9)
+                    <br/>
 
+                    @else
+                    <br/>
+                    <br/>
+                    @endif
                     <div class="row justify-content-center align-items-right" style="position: relative; bottom: 25px;">
                         <a href="{{ route('post.show', $sabor) }}" class=" p-4"><input type="button" value="Saber más" class="p-2"></input></a>
                     </div>
