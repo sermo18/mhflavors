38042dab454c61e56f42b51e1b51a160da775a43
sergio <sermo14.sm@gmail.com>
2021-05-19 18:41:30 +0200

    estilos

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -68,7 +68,7 @@
                 <a href="#" class="btn btn-dark">Contacta con nosotros</a>
             </div>
         </div>
-        <div class="col-12  sandback">
+        <div class="col-12 my-4 sandback align-content-center">
             <div class="row ">
                 <div class="col-12">
                     <h1 class="text-white text-center">Comunidad</h1>
