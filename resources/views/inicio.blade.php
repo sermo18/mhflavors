44f67def4d96921cae43f927233b1a5c37dde246
sergio <sermo14.sm@gmail.com>
2021-05-19 19:57:23 +0200

    estilos

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -35,7 +35,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-12">
+        <div class="col-12"  style="width: 100%; height: 700px !important;">
         <div class="row ">
             <div class="col-12 col-md-6">
                 <div class="card  noBorde ">
