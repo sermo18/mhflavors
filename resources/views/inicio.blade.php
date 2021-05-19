f7e8c51811c0ab4136c8c8e70fdd386282c9dd4d
sergio <sermo14.sm@gmail.com>
2021-05-19 20:27:15 +0200

    estilos

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -36,42 +36,40 @@
             </div>
         </div>
         <div class="col-12">
-            
+            <div class="row ">
                 <div class="col-12 col-md-6 serviceback">
+                    
                     <div >
-                        <h1 class="p-4">Servicios</h1>
-                    </div>
-                    <div >
-                        <h5 class="p-2">¿Que ofrecemos?</h5>
-                        <p class=" p-2">
+                    <h1 class=" text-white p-4">Servicios</h1>
+                        <h5 class="text-white p-2">¿Que ofrecemos?</h5>
+                        <p class="text-white p-2">
                             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
                             industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
                             and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
                             leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
                             with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
                             publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
-                        <p class="text-center"><a href="#" class="btn btn-dark">Contacta con nosotros</a></p>
+                        <p class="text-center"><a href="#" class="btn btn-light">Contacta con nosotros</a></p>
                     </div>
                 </div>
 
 
                 <div class="col-12 col-md-6 contactoback">
+                  
                     <div >
-                        <h1 class="p-4">Contacto</h1>
-                    </div>
-                    <div >
-                        <h5 class="p-2">¿Necesitas Algo?</h5>
-                        <p class=" p-2">
+                    <h1 class=" text-white p-4">Contacto</h1>
+                        <h5 class= "text-white p-2">¿Necesitas Algo?</h5>
+                        <p class="text-white p-2">
                             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
                             industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
                             and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
                             leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
                             with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
                             publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
-                        <p class="text-center"><a href="#" class="btn btn-dark">Contacta con nosotros</a></p>
+                        <p class="text-center"><a href="#" class="btn btn-light">Contacta con nosotros</a></p>
                     </div>
                 </div>
-            
+            </div>
         </div>
         <div class="col-12 sandback ">
             <div class="row ">
