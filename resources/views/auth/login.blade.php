74f1b283cda91163ab473d74bec41787a8fdb2fc
sergio <sermo14.sm@gmail.com>
2021-05-20 19:33:46 +0200

    add Procfileç

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,31 +1,30 @@
 @extends('plantilla')
 @section('titulo', 'Login')
 @section('contenido')
-<div class="container">
-<div class="row m-4"> 
+<div class="container-fluid">
+
+    <div class="col-12 text-center  loginback">
+    <div class="row p-4">
         <h1>Login</h1>
     </div>
-@if (!empty($error))
-<div class="text-danger">
-{{ $error }}
-</div>
-@endif
-<form action="{{ route('auth.login') }}" method="POST">
-@csrf
-<div class="form-group ">
-<label for="login">Login:</label>
-<input type="text" class="form-control shadow p-3"
-name="login" id="login" />
-</div>
-<div class="form-group">
-<label for="password">Password:</label>
-<input type="password" class="form-control shadow p-3"
-name="password" id="password" />
-</div>
-<input type="submit" name="enviar" value="Enviar"
-class="btn btn-dark btn-block">
-<a href="{{ route('auth.registro') }}" class="m-1 "><input type="button"
-value="Registrate" class="btn btn-light btn-block"></input></a>
-</form>
+    @if (!empty($error))
+    <div class="text-danger">
+        {{ $error }}
+    </div>
+    @endif
+    <form action="{{ route('auth.login') }}" class="px-4" style="background-color: white;" method="POST">
+        @csrf
+        <div class="form-group ">
+            <label for="login">Login:</label>
+            <input type="text" class="form-control shadow p-3" name="login" id="login" />
+        </div>
+        <div class="form-group">
+            <label for="password">Password:</label>
+            <input type="password" class="form-control shadow p-3" name="password" id="password" />
+        </div>
+        <input type="submit" name="enviar" value="Enviar" class="btn btn-dark btn-block ">
+        <a href="{{ route('auth.registro') }}" class="m-1 "><input type="button" value="Registrate" class="btn btn-light btn-block"></input></a>
+    </form>
+    </div>
 </div>
 @endsection
