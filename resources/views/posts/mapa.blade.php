f29dc6aa84b5d15d1f4fd0de1ea5c7df2be5fb28
sergio <sermo14.sm@gmail.com>
2021-05-20 20:28:35 +0200

    add Procfileç

--- resources/views/posts/mapa.blade.php
+++ resources/views/posts/mapa.blade.php
@@ -6,13 +6,14 @@
     <div class="row ">
 
         <div class="col-12  text-center mapaback">
-
-            <h1 class="text-light text-center col-12">Principales Lounges</h1>
-
-            <div id="map-container " class=" map-container mb-4 col-8">
-                <!-- This page show us a map with the most popular lounges that we can go to smoke -->
-                <iframe class="mb-4 shadow p-3" src="https://www.google.com/maps/d/embed?mid=1LmlfBYmV4ljXxjqELdmLOBscH02e-GxV&hl=es" width="100%" height="80%" frameborder="0" style="border:0"></iframe>
-
+            <div class="col-12">
+                <h1 class="text-light text-center">Principales Lounges</h1>
+            </div>
+            <div class="col-8">
+                <div id="map-container " class=" map-container mb-4">
+                    <!-- This page show us a map with the most popular lounges that we can go to smoke -->
+                    <iframe class="mb-4 shadow p-3" src="https://www.google.com/maps/d/embed?mid=1LmlfBYmV4ljXxjqELdmLOBscH02e-GxV&hl=es" width="100%" height="80%" frameborder="0" style="border:0"></iframe>
+                </div>
             </div>
         </div>
     </div>
