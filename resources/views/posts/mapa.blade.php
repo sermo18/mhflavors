762546237f2ccbdc42db61dd6d48cb00e265ed43
sergio <sermo14.sm@gmail.com>
2021-05-20 20:19:01 +0200

    add Procfileç

--- resources/views/posts/mapa.blade.php
+++ resources/views/posts/mapa.blade.php
@@ -5,7 +5,8 @@
 <div class="container-fluid ">
 
 
-    <div class="row text-center mapaback">
+    <div class="row">
+    <div class="col-12  text-center mapaback">
 
         <h1 class="text-light text-center col-12">Principales Lounges</h1>
         <div id="map-container " class=" map-container mb-4 col-8" >
@@ -13,7 +14,7 @@
             <iframe class="mb-4 shadow p-3" src="https://www.google.com/maps/d/embed?mid=1LmlfBYmV4ljXxjqELdmLOBscH02e-GxV&hl=es" width="100%" height="80%" frameborder="0" style="border:0"></iframe>
         </div>
 
-
+        </div>
     </div>
 </div>
 @endsection
