c49d561b9fbc465365f3e031c42dae1d0435a13a
sergio <sermo14.sm@gmail.com>
2021-05-20 18:16:31 +0200

    add Procfileç

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -19,7 +19,7 @@
     <!--Grid row-->
     <div class="row">
       <!--Grid column-->
-      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+      <div class="col-lg-4 col-md-12 mb-4 mb-md-0">
         <h5 class="text-uppercase">MHFLAVORS</h5>
 
         <p>
@@ -31,25 +31,25 @@
       <!--Grid column-->
 
       <!--Grid column-->
-      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+      <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
         <h5 class="text-uppercase">CATEGORÍAS DESTACADAS</h5>
 
         <ul class="list-unstyled mb-0">
           <li>
-            <a href="#!" class="text-white">Top Sabores</a>
+            <a href="#!" >Top Sabores</a>
           </li>
           <li>
-            <a href="#!" class="text-white">Nuestras Mezclas</a>
+            <a href="#!" >Nuestras Mezclas</a>
           </li>
           <li>
-            <a href="#!" class="text-white">Tus Mezclas Favoritas</a>
+            <a href="#!" >Tus Mezclas Favoritas</a>
           </li>
         </ul>
       </div>
       <!--Grid column-->
 
       <!--Grid column-->
-      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+      <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
         <h5 class="text-uppercase mb-0">SOBRE NOSOTROS</h5>
 
         <ul class="list-unstyled">
