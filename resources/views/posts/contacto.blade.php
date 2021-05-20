e6bc2ff0e046aec73e9076f29952702e577c5b0d
sergio <sermo14.sm@gmail.com>
2021-05-20 17:51:50 +0200

    add Procfileç

--- resources/views/posts/contacto.blade.php
+++ resources/views/posts/contacto.blade.php
@@ -8,6 +8,7 @@
 <div class="row">
         <div class="col-12 text-center contactoback">
         <form>
+        <h1 class="text-white">Contacto</h1>
         <div class="form-group row" style="background-color: white;">
             <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
             <div class="col-sm-10">
