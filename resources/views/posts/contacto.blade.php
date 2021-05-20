43461f4877fa435a063760875cdbf843a65031dc
sergio <sermo14.sm@gmail.com>
2021-05-20 17:51:01 +0200

    add Procfileç

--- resources/views/posts/contacto.blade.php
+++ resources/views/posts/contacto.blade.php
@@ -3,12 +3,12 @@
 @section('contenido')
 <link href="{{ secure_asset(mix('css/app.css')) }}" rel="stylesheet">
 <!-- This is the contact page -->
-<div class="container" height="100%">
+<div class="container-fluid" height="100%">
 
 <div class="row">
-        <div class="col-12 text-center img-header">
+        <div class="col-12 text-center contactoback">
         <form>
-        <div class="form-group row">
+        <div class="form-group row" style="background-color: white;">
             <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
             <div class="col-sm-10">
                 <input type="email" class="form-control shadow p-3" id="inputEmail3" placeholder="tuemail@example.com">
@@ -49,5 +49,6 @@
     </div>
 
 
+</div>
 </div>
 @endsection
