401343b035f38e7fe75e93186fe2af4ddfd5d260
sergio <sermo14.sm@gmail.com>
2021-05-21 17:34:57 +0200

    estilos

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -84,10 +84,9 @@
         </div>
         <div class="col-12 sandback ">
             <div class="row ">
+                
                 <div class="col-12">
-                    <h1 class="text-white text-center mb-4">Comunidad</h1>
-                </div>
-                <div class="col-12">
+                <h1 class="text-white text-center mb-4">Comunidad</h1>
                     <div class="row">
                         <div class="card col-6 col-md-3 noBorde" style="border: none; border-radius: 0; background-color: transparent;">
                             <div class="card-body text-center noBorde ">
