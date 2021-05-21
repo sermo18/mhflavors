082e52756a59f3e644c4e6bb9df5b4ff7926c579
sergio <sermo14.sm@gmail.com>
2021-05-21 19:27:23 +0200

    estilos

--- resources/views/mezclas/index.blade.php
+++ resources/views/mezclas/index.blade.php
@@ -3,11 +3,12 @@
 @section('contenido')
 <script src="{{ asset('js/ponerImagenes.js') }}"></script>
 
-<div class="container-fluid">
+<div class="container-fluid maderitaBack">
+
 <div class="table-responsive">
 <!-- This page shows all mixings that the users had created -->
     <table class="table table-striped">
-        <div class="row m-4 text-center">
+        <div class="row m-4 text-center align-content-center justify-content-center">
             <h1 class="text-center">Listado de Mezclas</h1>
         </div>
         <tr>
