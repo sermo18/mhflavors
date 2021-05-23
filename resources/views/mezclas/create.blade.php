f19afcda133d55c90f856c9feef173791d570834
sergio <sermo14.sm@gmail.com>
2021-05-23 20:50:11 +0200

    add Procfileç

--- resources/views/mezclas/create.blade.php
+++ resources/views/mezclas/create.blade.php
@@ -25,9 +25,9 @@
                             </option>
                             @endforeach
                         </datalist>
-                        <h2 class="card-text py-4">Elige el sabor e indica la cantidad que quieres ponerle.</h2>
+                        <p class="card-text py-4">Elige el sabor e indica la cantidad que quieres ponerle.</p>
                         <div class="form-group py-4">
-                            <label><h2>Porcentaje del primer sabor:</h2></label>
+                            <label>Porcentaje del primer sabor:</label>
                             <input type="number" class="form-control text-center mt-3" name="porcentaje{{$i}}" id="porcentaje{{$i}}" value="" min="0" max="100" step="10">
                         </div>
 
@@ -47,9 +47,9 @@
                         @endforeach
                     </datalist>
 
-                    <h2 class="card-text py-4">Elige el sabor e indica la cantidad que quieres ponerle.</h2>
+                    <p class="card-text py-4">Elige el sabor e indica la cantidad que quieres ponerle.</p>
                     <div class="form-group py-4">
-                        <label><h2>Porcentaje del primer sabor:</h2></label>
+                        <label>Porcentaje del primer sabor:</label>
                         <input type="number" class="form-control text-center mt-3" name="porcentaje{{$i}}" id="porcentaje{{$i}}" value="" min="0" max="100" step="10">
                     </div>
 
@@ -68,9 +68,9 @@
                 @endforeach
             </datalist>
 
-            <h2 class="card-text py-4">Elige el sabor e indica la cantidad que quieres ponerle.</h2>
+            <p class="card-text py-4">Elige el sabor e indica la cantidad que quieres ponerle.</p>
             <div class="form-group py-4">
-                <label><h2>Porcentaje del primer sabor:</h2></label>
+                <label>Porcentaje del primer sabor:</label>
                 <input type="number" class="form-control text-center mt-3" name="porcentaje{{$i}}" id="porcentaje{{$i}}" value="" min="0" max="100" step="10">
             </div>
 
