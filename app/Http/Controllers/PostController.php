71f79f93ae723d8cf1f41f068f4d1be970215ba9
sergio <sermo14.sm@gmail.com>
2021-05-24 19:55:54 +0200

    estilos

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,7 +7,7 @@ use App\Models\Sabor;
 use App\Models\Mezcla;
 use App\Models\Usuario;
 use App\Http\Requests\PostRequest;
-
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Pagination\LengthAwarePaginator;
