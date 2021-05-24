d44d89243a09f6ce23ff1d5d49e39535995d2efd
sergio <sermo14.sm@gmail.com>
2021-05-24 17:13:21 +0200

    estilos

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -82,7 +82,7 @@
         </div>
 
         @forelse($saboresmarca as $sabormarca)
-        <a class="col-3" href="{{ route('post.show', $sabormarca) }}">
+        <a class="col-3" href="{{ route('post.show', $sabormarca) }}" style="text-decoration: none;">
         <div class="card text-left ponerImagenes  agrandar" style="height: 200px; width: 200px;">
             <div class="card-body text-white " >
                 <h3 class="card-title text-left" style="height: 30px;">{{ $sabormarca->nombre }}</h3>
