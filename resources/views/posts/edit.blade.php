547fc97768ccc1b6bbe739c590f1e94bb4010a63
sergio <sermo14.sm@gmail.com>
2021-05-25 21:10:05 +0200

    add Procfileç

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -5,7 +5,7 @@
 <div class="container-fluid" style="height: 100vh;">
 <!-- This view it's to valorate the flavors -->
 <div class="row justify-content-md-center align-items-center pt-3">
-    <div class="card text-left ponerImagenes col-6" style="height:100%;">
+    <div class="card text-left ponerImagenes col-12 col-md-4" style="height:100vh;">
         <div class="card-body text-white ">
             <h3 class="card-title text-left" style="height: 70px;">{{ $sabor->nombre }}</h3>
             <h5 class="card-text p-3">Descripcion: {{$sabor->descripcion }}</h5>
