2e62f76af0eabbe1b066189beff9fa81edc78abc
sergio <sermo14.sm@gmail.com>
2021-05-25 18:02:22 +0200

    add Procfileç

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -104,7 +104,7 @@
         @if($post->sabor_id == $sabor->id)
         <div class="card">
             <div class="card-header">
-            @if({{ $post->usuario->nombre }})
+            @if( $post->usuario->nombre)
             {{ $post->usuario->nombre }}
             @else
             Anónimo
