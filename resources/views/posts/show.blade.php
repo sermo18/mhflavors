3e9a7e7b3dbeb22251c75f51cc03a3641436ea32
sergio <sermo14.sm@gmail.com>
2021-05-25 17:38:04 +0200

    add Procfileç

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -76,7 +76,7 @@
         @endif
     </div>
 </div>
-<div class="row mt-3">
+<div class="row">
     <div class="col-12">
         <form action="{{ route('post.store') }}" method="GET">
             @csrf
@@ -84,16 +84,16 @@
                 <label for="exampleFormControlTextarea1">Comentarios:</label>
                 <textarea class="form-control" id="exampleFormControlTextarea1" name="contenido" id="contenido" rows="3"></textarea>
             </div>
-            
+
             <div class="form-group">
                 <input type="hidden" class="form-control" name="usuario" id="usuario" value="{{$userid}}">
             </div>
             <div class="form-group">
                 <input type="hidden" class="form-control" name="idsabor" id="idsabor" value="{{$sabor->id}}">
             </div>
-           
+
             <input type="submit" class="btn btn-secondary" value="Enviar" />
-            
+
         </form>
     </div>
 </div>
