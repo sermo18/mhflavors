15ca7fcc01a303b400c6897367efa2cab179a859
sergio <sermo14.sm@gmail.com>
2021-05-28 20:09:17 +0200

    estilos

--- resources/views/mezclas/create.blade.php
+++ resources/views/mezclas/create.blade.php
@@ -2,46 +2,47 @@
 @section('titulo', 'Nueva Mezcla')
 @section('contenido')
 <div class="container-fluid " style="height: 100vh;">
-    <div class="imagenMezclaCreada ">
-        <div class="row m-4">
-            <h1>Crea tu nueva Mezcla</h1>
-        </div>
-        <form action="{{ route('mezclas.store') }}" method="GET">
-            @csrf
+    
+    <form action="{{ route('mezclas.store') }}" method="GET">
+        @csrf
 
-            <!-- This page its for create a new mixing  -->
-            <div class="col-12">
-                @for ($i = 1; $i < 4; $i++) <div class="col-12 col-md-4 mb-3 ">
+        <!-- This page its for create a new mixing  -->
+        <div class="row imagenMezclaCreada" >
+            
+        <div class="col-12 text-light text-center">
+        <h1>Crea tu nueva Mezcla</h1>
+    </div>
+            @for ($i = 1; $i < 4; $i++) <div class="col-12 col-md-4 mb-3 ">
 
-                    <div class="card text-center shadow p-3 saborBack{{$i}} ">
-                        @if($i == 1)
-
-
-                        <div class="card-body text-light py-4 ">
-                            <p class="card-text py-4 my-2">Elige el sabor e indica la cantidad que quieres ponerle.</p>
-                            <input class="form-control  buscador1 my-2" list="datalistOptions" name="sabor{{$i}}" id="sabor{{$i}}" placeholder="Type to search...">
-                            <datalist id="datalistOptions">
-                                @foreach ($sabores as $sabor)
-                                <option value="{{ $sabor->id }}">
-                                    {{ $sabor->nombre }} de {{ $sabor->marca}}
-                                </option>
-                                @endforeach
-                            </datalist>
-                            <div class="form-group py-4 my-2">
-                                <label>Porcentaje del primer sabor:</label>
-                                <input type="number" class="form-control text-center my-2" name="porcentaje{{$i}}" id="porcentaje{{$i}}" value="" min="0" max="100" step="10">
-                            </div>
+                <div class="card text-center shadow p-3 saborBack{{$i}} ">
+                    @if($i == 1)
 
 
+                    <div class="card-body text-light py-4 ">
+                        <p class="card-text py-4 my-2">Elige el sabor e indica la cantidad que quieres ponerle.</p>
+                        <input class="form-control  buscador1 my-2" list="datalistOptions" name="sabor{{$i}}" id="sabor{{$i}}" placeholder="Type to search...">
+                        <datalist id="datalistOptions">
+                            @foreach ($sabores as $sabor)
+                            <option value="{{ $sabor->id }}">
+                                {{ $sabor->nombre }} de {{ $sabor->marca}}
+                            </option>
+                            @endforeach
+                        </datalist>
+                        <div class="form-group py-4 my-2">
+                            <label>Porcentaje del primer sabor:</label>
+                            <input type="number" class="form-control text-center my-2" name="porcentaje{{$i}}" id="porcentaje{{$i}}" value="" min="0" max="100" step="10">
                         </div>
+
 
                     </div>
 
-                    @elseif($i == 2)
-                    <div class="card-body text-light py-4">
-                        <p class="card-text py-4 my-2">Elige el sabor e indica la cantidad que quieres ponerle.</p>
+                </div>
 
-                        <input class="form-control  buscador1 my-2" list="datalistOptions2" name="sabor{{$i}}" id="sabor{{$i}}" placeholder="Type to search...">
+                @elseif($i == 2)
+                <div class="card-body text-light py-4">
+                    <p class="card-text py-4 my-2">Elige el sabor e indica la cantidad que quieres ponerle.</p>
+
+                    <input class="form-control  buscador1 my-2" list="datalistOptions2" name="sabor{{$i}}" id="sabor{{$i}}" placeholder="Type to search...">
                         <datalist id="datalistOptions2">
                             @foreach ($sabores as $sabor)
                             <option value="{{ $sabor->id }}">
@@ -50,38 +51,37 @@
                             @endforeach
                         </datalist>
 
-                        <div class="form-group py-4 my-2">
-                            <label>Porcentaje del primer sabor:</label>
-                            <input type="number" class="form-control text-center my-2" name="porcentaje{{$i}}" id="porcentaje{{$i}}" value="" min="0" max="100" step="10">
-                        </div>
-
+                    <div class="form-group py-4 my-2">
+                        <label>Porcentaje del primer sabor:</label>
+                        <input type="number" class="form-control text-center my-2" name="porcentaje{{$i}}" id="porcentaje{{$i}}" value="" min="0" max="100" step="10">
                     </div>
-            </div>
-            @elseif($i == 3)
 
-
-            <div class="card-body text-light py-4">
-                <p class="card-text py-4 my-2">Elige el sabor e indica la cantidad que quieres ponerle.</p>
-
-                <input class="form-control  buscador1 my-2" list="datalistOptions3" name="sabor{{$i}}" id="sabor{{$i}}" placeholder="Type to search...">
-                <datalist id="datalistOptions3">
-                    @foreach ($sabores as $sabor)
-                    <option value="{{ $sabor->id }}">
-                        {{ $sabor->nombre }} de {{ $sabor->marca}}
-                    </option>
-                    @endforeach
-                </datalist>
-
-                <div class="form-group py-4 my-2">
-                    <label>Porcentaje del primer sabor:</label>
-                    <input type="number" class="form-control text-center my-2" name="porcentaje{{$i}}" id="porcentaje{{$i}}" value="" min="0" max="100" step="10">
                 </div>
+        </div>
+        @elseif($i == 3)
 
 
+        <div class="card-body text-light py-4">
+            <p class="card-text py-4 my-2">Elige el sabor e indica la cantidad que quieres ponerle.</p>
+
+            <input class="form-control  buscador1 my-2" list="datalistOptions3" name="sabor{{$i}}" id="sabor{{$i}}" placeholder="Type to search...">
+            <datalist id="datalistOptions3">
+                @foreach ($sabores as $sabor)
+                <option value="{{ $sabor->id }}">
+                    {{ $sabor->nombre }} de {{ $sabor->marca}}
+                </option>
+                @endforeach
+            </datalist>
+
+            <div class="form-group py-4 my-2">
+                <label>Porcentaje del primer sabor:</label>
+                <input type="number" class="form-control text-center my-2" name="porcentaje{{$i}}" id="porcentaje{{$i}}" value="" min="0" max="100" step="10">
             </div>
-    </div>
-    @endif
+
+
+        </div>
 </div>
+@endif
 </div>
 @endfor
 </div>
