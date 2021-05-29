a710f07d180c5f1d07183ffc13faa62e7c7ed68b
sergio <sermo14.sm@gmail.com>
2021-05-29 12:08:21 +0200

    estilos

--- resources/views/posts/noticias.blade.php
+++ resources/views/posts/noticias.blade.php
@@ -2,22 +2,34 @@
 @section('titulo', 'Ultimas Noticias')
 @section('contenido')
 <div class="container-fluid">
-    
+
     <!-- This page show us the last news -->
     <div class="row p-4 notiback">
         <div class="col-12">
-        <h1>Últimas Noticias</h1>
+            <h1>Últimas Noticias</h1>
         </div>
         <div class="col-12">
-            <div class="card">
-                <img src="https://www.diariodecadiz.es/2021/05/14/cadiz/cachimbas-moda-ano-pandemia_1573952634_138491982_667x375.jpg" class="card-img" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Las cachimbas, la moda de un año de pandemia
-</h5>
-                    <p class="card-text">Ahora los fumadores de cachimbas desean tener en su propio hogar cachimbas, accesorios y tabacos para fumar sin salir de casa y pasar mucho mejor el encierro por la pandemia...</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    <a href="https://www.diariodecadiz.es/cadiz/cachimbas-moda-ano-pandemia_0_1573642960.html" class="btn btn-dark mt-2">Saber Más</a>
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="row no-gutters">
+                            <div class="col-md-4">
+                                <img src="https://www.diariodecadiz.es/2021/05/14/cadiz/cachimbas-moda-ano-pandemia_1573952634_138491982_667x375.jpg" class="card-img" alt="...">
+                            </div>
+                            <div class="col-md-8">
 
+                                <div class="card-body">
+                                    <h5 class="card-title">Las cachimbas, la moda de un año de pandemia
+                                    </h5>
+                                    <p class="card-text">Ahora los fumadores de cachimbas desean tener en su propio hogar cachimbas, accesorios y tabacos para fumar sin salir de casa y pasar mucho mejor el encierro por la pandemia...</p>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    <a href="https://www.diariodecadiz.es/cadiz/cachimbas-moda-ano-pandemia_0_1573642960.html" class="btn btn-dark mt-2">Saber Más</a>
+
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
 
                 </div>
             </div>
@@ -27,13 +39,13 @@
                 <div class="col-12">
                     <div class="card ">
                         <div class="row no-gutters">
-                            <div class="col-md-7">
+                            <div class="col-md-4">
                                 <img src="https://www.madridiario.es/fotos/1/231545_unnamed-71_thumb_722.jpg" class="card-img" alt="...">
                             </div>
-                            <div class="col-md-5">
+                            <div class="col-md-8">
                                 <div class="card-body">
                                     <h5 class="card-title">Las cachimbas, una moda incipiente
-</h5>
+                                    </h5>
 
                                     <p class="card-text">Se está incrementando el número de fumadores...</p>
 
@@ -49,10 +61,10 @@
                 <div class="col-12 ">
                     <div class="card ">
                         <div class="row no-gutters">
-                            <div class="col-md-7">
+                            <div class="col-md-4">
                                 <img src="https://static.eldiario.es/clip/d960ea2b-1cd0-4930-ba58-9d6a2a7be296_16-9-aspect-ratio_default_0.jpg" class="card-img" alt="...">
                             </div>
-                            <div class="col-md-5">
+                            <div class="col-md-8">
                                 <div class="card-body">
                                     <h5 class="card-title">El negocio de la cachimba florece en España al borde de la ley</h5>
                                     <p class="card-text">A falta de cerrar los datos de diciembre...</p>
@@ -72,7 +84,7 @@
     <div class="row mt-4">
         <div class="col-12">
             <div class="card mb-3">
-                <img src="https://21noticias.com/wp-content/uploads/2021/02/91e4ccec5e1a902ba74fd6c66373a852-400x280.jpg"  style="height: 200px;" class="card-img" alt="...">
+                <img src="https://21noticias.com/wp-content/uploads/2021/02/91e4ccec5e1a902ba74fd6c66373a852-400x280.jpg" style="height: 200px;" class="card-img" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -105,9 +117,9 @@
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
 
-                 <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
 
-                 <a href="#" class="btn btn-dark mt-2">Saber Más</a>
+                    <a href="#" class="btn btn-dark mt-2">Saber Más</a>
 
                 </div>
             </div>
