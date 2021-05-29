2df1003d61d61828739017c5a43e9c0f128acf4a
sergio <sermo14.sm@gmail.com>
2021-05-29 13:07:48 +0200

    estilos

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -116,7 +116,7 @@
                         {{$post->contenido}}
                     </p>
                     <footer class="blockquote-footer">
-                        Fecha:<cite title="Source Title">{{date('Y-m-d H:i:s')}}</cite>
+                        fecha: <cite title="Source Title">{{date('Y-m-d H:i:s')}}</cite>
                     </footer>
                 </blockquote>
             </div>
