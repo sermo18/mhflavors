b3854aad326f8bbf9aff126a3dc5676a12842db6
sergio <sermo14.sm@gmail.com>
2021-05-29 11:43:53 +0200

    estilos

--- resources/views/posts/mapa.blade.php
+++ resources/views/posts/mapa.blade.php
@@ -4,13 +4,13 @@
 <div class="container-fluid p-0 m-0">
 
 
-    <div class="row mapaback">
+    <div class="row mapaback p-0 m-0">
         <h1 class="text-light text-center pt-4 m-4">Principales Lounges</h1>
         <div class="col-12  text-center ">
             <div class="col-12">
                 <div id="map-container " class=" map-container mb-4">
                     <!-- This page show us a map with the most popular lounges that we can go to smoke -->
-                    <iframe class="mb-4 shadow p-3" src="https://www.google.com/maps/d/embed?mid=1LmlfBYmV4ljXxjqELdmLOBscH02e-GxV&hl=es" width="80%" height="50vh" frameborder="0" style="border:0"></iframe>
+                    <iframe class="mb-4 shadow p-3" src="https://www.google.com/maps/d/embed?mid=1LmlfBYmV4ljXxjqELdmLOBscH02e-GxV&hl=es" width="80%" height="500px" frameborder="0" style="border:0"></iframe>
                 </div>
             </div>
 
