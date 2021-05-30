b5e770d9249491ed4f238f1240b3d306c5e16e39
sergio <sermo14.sm@gmail.com>
2021-05-30 12:57:01 +0200

    estilos

--- resources/views/mezclas/index.blade.php
+++ resources/views/mezclas/index.blade.php
@@ -130,8 +130,6 @@
                     @if( auth()->user()->rol === 'premium')
                         @foreach ($mezclasFavoritas as $mezclaFavorita)
                             @if($mezclaFavorita->mezclas_id == $mezcla->id && $mezclaFavorita->usuario->id == $userid)
-                                
-
                     <a class="m-1 " title="Añadir a favoritas"><button type="button" class="btn btn-dark px-3"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-heart-fill" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
                             </svg></button></a>
@@ -145,8 +143,7 @@
                    
                             @endif
                          @endforeach
-
-
+                         @if(!$mezcla->usuario_id == $userid)
                          <a href="{{ route('mezclas.añadir', $mezcla) }}" class="m-1 " title="Añadir a favoritas"><button type="button" class="btn btn-dark px-3"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-heart" viewBox="0 0 16 16">
                                 <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
                             </svg></button></a>
@@ -157,6 +154,10 @@
                     <a href="{{ route('mezclas.valorar', $mezcla) }}" class="m-1 " title="Valorar"><button type="button" class="btn btn-dark px-3"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
                                 <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
                             </svg></button></a>
+                            @else
+                   
+                            @endif
+                 
                     @else
 
                     <a class="m-1 " title="Añadir a favoritas"><button type="button" class="btn btn-dark px-3" data-toggle="modal" data-target="#modalPremium"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-heart" viewBox="0 0 16 16">
