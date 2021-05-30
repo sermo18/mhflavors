09272d6742b6d89d1e01952d3a9b4da5807deead
sergio <sermo14.sm@gmail.com>
2021-05-30 11:24:07 +0200

    estilos

--- resources/views/mezclas/index.blade.php
+++ resources/views/mezclas/index.blade.php
@@ -130,7 +130,7 @@
                     @if( auth()->user()->rol === 'premium')
                     @foreach ($mezclasFavoritas as $mezclaFavorita)
                     @if($mezclaFavorita->usuario->id == $userid)
-                    @if($mezclaFavorita->mezcla == $mezcla)
+                    @if($mezclaFavorita->mezclas_id == $mezcla->id)
                     <a class="m-1 " title="Añadir a favoritas"><button type="button" class="btn btn-dark px-3"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-heart-fill" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
                             </svg></button></a>
