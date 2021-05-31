f67105d68221b71be85eab7f2ff6c4bdc34dd708
sergio <sermo14.sm@gmail.com>
2021-05-31 21:15:24 +0200

    Subiendo a heroku

--- resources/views/mezclas/create.blade.php
+++ resources/views/mezclas/create.blade.php
@@ -12,7 +12,7 @@
             <form action="{{ route('mezclas.store') }}" class="col-12" method="GET">
             @csrf
                 <div class="row text-center align-content-center justify-content-center">
-                    <div class="col-12 text-light text-center mb-4">
+                    <div class="col-12 text-light text-center my-4">
                     <h1>Crea tu nueva Mezcla</h1>
                     </div>
                 @for ($i = 1; $i < 4; $i++)
