6541ce191a975b879b6af927c037fbfbca54d1e3
sergio <sermo14.sm@gmail.com>
2021-05-31 19:02:58 +0200

    Subiendo a heroku

--- resources/views/posts/noticias.blade.php
+++ resources/views/posts/noticias.blade.php
@@ -8,7 +8,7 @@
         <div class="col-12 p-4">
             <h1 class="text-white">Últimas Noticias</h1>
         </div>
-        <div class="col-12 m-4">
+        <div class="col-12 my-4">
             <div class="row">
                 <div class="col-12">
                     <div class="card  text-light" style=" background: rgba(0,0,0,0.75);">
@@ -34,7 +34,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-12 m-4">
+        <div class="col-12 my-4">
             <div class="row">
                 <div class="col-12">
                     <div class="card  text-light" style=" background: rgba(0,0,0,0.75);">
@@ -58,7 +58,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-12 m-4 ">
+                <div class="col-12 my-4 ">
                     <div class="card  text-light" style=" background: rgba(0,0,0,0.75);">
                         <div class="row no-gutters">
                             <div class="col-md-4">
@@ -78,7 +78,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-12 m-4 ">
+                <div class="col-12 my-4 ">
                     <div class="card  text-light" style=" background: rgba(0,0,0,0.75);">
                         <div class="row no-gutters">
                             <div class="col-md-4">
@@ -98,7 +98,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-12 m-4">
+                <div class="col-12 my-4">
                     <div class="card text-light" style=" background: rgba(0,0,0,0.75);">
                         <div class="row no-gutters">
                             <div class="col-md-4">
