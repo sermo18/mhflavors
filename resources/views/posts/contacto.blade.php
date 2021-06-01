f91198755d7e2c4f793828bee261adb078d29fb6
sergio <sermo14.sm@gmail.com>
2021-06-01 17:39:16 +0200

    primer commit local

--- resources/views/posts/contacto.blade.php
+++ resources/views/posts/contacto.blade.php
@@ -6,8 +6,8 @@
 <div class="container-fluid">
 
     <div class="row">
-        <div class="col-12 text-center contactoback2">
-            <form class="px-4" style="background-color: white;opacity: 0.85; border-radius:22px;">
+        <div class="col-12 text-center contactoback2 p-4">
+            <form class="p-4 m-4 " style="background-color: white;opacity: 0.85; border-radius:22px;">
                 <h1 class="text-dark p-3">Contacto</h1>
                 <div class="form-group row p-3">
                     <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
