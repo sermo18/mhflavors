ec057ac485c7eafe7be1d539235a183046f5636d
sergio <sermo14.sm@gmail.com>
2021-06-01 16:46:20 +0200

    estilos

--- resources/views/mezclas/create.blade.php
+++ resources/views/mezclas/create.blade.php
@@ -2,61 +2,64 @@
 @section('titulo', 'Nueva Mezcla')
 @section('contenido')
 <div class="container-fluid p-0 m-0">
-    
-        <!-- This page its for create a new mixing  -->
-        <div class="row p-4 m-0 imagenMezclaCreada">
 
-            <div class="col-12 text-light text-center mb-4">
-                <h1>Crea tu nueva Mezcla</h1>
-            </div>
-            <form action="{{ route('mezclas.store') }}" method="GET">
-        @csrf
+    <!-- This page its for create a new mixing  -->
+    <div class="row p-4 m-0 imagenMezclaCreada">
 
-            @for ($i = 1; $i < 4; $i++)
-            <div class="col-12 col-md-4 py-4">
+        <div class="col-12 text-light text-center mb-4">
+            <h1>Crea tu nueva Mezcla</h1>
+        </div>
+        <form action="{{ route('mezclas.store') }}" method="GET">
+            @csrf
+            <div class="col-12 py-4">
+                @for ($i = 1; $i < 4; $i++) 
+                <div class="row">
+                    <div class="col-12 col-md-4">
 
-                <div class="card text-center" style="background: rgba(0,0,0,0.4);">
-        
-                    <div class="card-body text-light py-4 ">
-                        <p class="card-text py-4 my-2">Elige el sabor:</p>
-                        <input class="form-control  buscador1 my-2" list="datalistOptions" name="sabor{{$i}}" id="sabor{{$i}}" placeholder="Type to search...">
-                        <datalist id="datalistOptions">
-                            @foreach ($sabores as $sabor)
-                            <option value="{{ $sabor->id }}">
-                                {{ $sabor->nombre }} de {{ $sabor->marca}}
-                            </option>
-                            @endforeach
-                        </datalist>
-                        <div class="form-group py-4 my-2">
-                            <label>Cantidad de sabor:</label>
-                            <input type="number" class="form-control text-center my-2" name="porcentaje{{$i}}" id="porcentaje{{$i}}" value="" min="0" max="100" step="10">
+                        <div class="card text-center" style="background: rgba(0,0,0,0.4);">
+
+                            <div class="card-body text-light py-4 ">
+                                <p class="card-text py-4 my-2">Elige el sabor:</p>
+                                <input class="form-control  buscador1 my-2" list="datalistOptions" name="sabor{{$i}}" id="sabor{{$i}}" placeholder="Type to search...">
+                                <datalist id="datalistOptions">
+                                    @foreach ($sabores as $sabor)
+                                    <option value="{{ $sabor->id }}">
+                                        {{ $sabor->nombre }} de {{ $sabor->marca}}
+                                    </option>
+                                    @endforeach
+                                </datalist>
+                                <div class="form-group py-4 my-2">
+                                    <label>Cantidad de sabor:</label>
+                                    <input type="number" class="form-control text-center my-2" name="porcentaje{{$i}}" id="porcentaje{{$i}}" value="" min="0" max="100" step="10">
+                                </div>
+
+
+                            </div>
+
                         </div>
-
 
                     </div>
 
                 </div>
-            </div>
-
             @endfor
-        
-
-            <div class="form-group col-12">
-                <input type="hidden" class="form-control" name="usuario" id="usuario" value="{{$userid}}">
             </div>
-        
 
-            @if(auth()->check())
-                <div class="my-1 col-12 align-content-center justify-content-center text-center">
+    <div class="form-group col-12">
+        <input type="hidden" class="form-control" name="usuario" id="usuario" value="{{$userid}}">
+    </div>
 
-                    <input type="submit" name="enviar" value="Enviar" class="btn btn-dark">
 
-                </div>
+    @if(auth()->check())
+    <div class="my-1 col-12 align-content-center justify-content-center text-center">
 
-            @endif
-            </form>
-        </div>
-   
+        <input type="submit" name="enviar" value="Enviar" class="btn btn-dark">
+
+    </div>
+
+    @endif
+    </form>
+</div>
+
 
 </div>
 @endsection
