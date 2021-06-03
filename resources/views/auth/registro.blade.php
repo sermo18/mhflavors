59a3c4efe079f1623d743382653fae188d2eaab9
sergio <sermo14.sm@gmail.com>
2021-06-03 17:50:56 +0200

    Subiendo a heroku

--- resources/views/auth/registro.blade.php
+++ resources/views/auth/registro.blade.php
@@ -12,7 +12,13 @@
                 @if ($errors->any())
                 <ul>
                     @foreach($errors->all() as $error)
-                    <li class="text-danger">{{ $error }}</li>
+                    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <strong>{{$error}}</strong>
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+
                     @endforeach
                 </ul>
                 @endif
