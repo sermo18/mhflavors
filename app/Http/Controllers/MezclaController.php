4725e41f269ab71d5479448fcaca76437eceeec6
sergio <sermo14.sm@gmail.com>
2021-06-05 18:40:15 +0200

    primer commit local

--- app/Http/Controllers/MezclaController.php
+++ app/Http/Controllers/MezclaController.php
@@ -22,7 +22,7 @@ class MezclaController extends Controller
     public function index()
     {
         // We take all mixings, flavors, users and favourite mixings and we pass this variables to the view index
-        $mezclas = DB::select( DB::raw("SELECT * FROM mezclas ORDER BY (valoracion/votos) DESC") )->paginate(20);
+        $mezclas = DB::select( DB::raw("SELECT * FROM mezclas ORDER BY (valoracion/votos) DESC") )->get()->paginate(20);
         ->paginate(20);
         $sabores= Sabor::get();
         $usuarios = Usuario::get();
