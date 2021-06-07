3410fbac8615d52c0117ace6045134df40a956fc
sergio <sermo14.sm@gmail.com>
2021-06-07 13:22:46 +0200

    Subiendo a heroku

--- resources/views/msg/index.blade.php
+++ resources/views/msg/index.blade.php
@@ -5,7 +5,7 @@
 
 <div class="container-fluid py-5 px-4">
 
-    <div class="row rounded-lg overflow-hidden shadow">
+    <div class="row rounded-lg overflow-hidden shadow" style="min-height: 100vh;">
         <!-- Users box-->
         <div class="col-12 px-0">
             <div class="bg-white">
@@ -86,7 +86,9 @@
                         <div class="input-group">
                             <input type="text" placeholder="Type a message" aria-describedby="button-addon2" class="form-control rounded-0 border-0 py-4 bg-light">
                             <div class="input-group-append">
-                                <button id="button-addon2" type="submit" class="btn btn-link"> <i class="fa fa-paper-plane"></i></button>
+                                <button id="button-addon2" type="submit" class="btn btn-dark"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-arrow-up-right-square" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z"/>
+</svg></button>
                             </div>
                         </div>
                     </form>
