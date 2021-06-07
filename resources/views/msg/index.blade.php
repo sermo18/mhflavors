c6b94e00f2a8e690037d42b5b1178e4e1bd5243d
sergio <sermo14.sm@gmail.com>
2021-06-07 13:42:39 +0200

    Subiendo a heroku

--- resources/views/msg/index.blade.php
+++ resources/views/msg/index.blade.php
@@ -18,8 +18,22 @@
                 <!-- Chat Box-->
                 <div class="col-12 px-0"  style="min-height: 100vh;">
                     <div class="px-4 py-5 chat-box bg-white">
-                        <!-- Sender Message-->
-                        <div class="media w-50 mb-3">
+                    @forelse($messages as $message)
+
+                    @if($message->usuario_id == auth()->user()->id )
+   <!-- Reciever Message-->
+   <div class="media w-50 ml-auto mb-3">
+                            <div class="media-body">
+                                <div class="bg-primary rounded py-2 px-3 mb-2">
+                                    <p class="text-small mb-0 text-white">Test which is a new approach to have all solutions</p>
+                                </div>
+                                <p class="small text-muted">12:00 PM | Aug 13</p>
+                            </div>
+                        </div>
+
+                        @else
+ <!-- Sender Message-->
+ <div class="media w-50 mb-3">
                             <p class="text-small mb-0 text-muted font-weight-bold">Antonio</p>
                             <div class="media-body ml-3">
                                 <div class="bg-light rounded py-2 px-3 mb-2">
@@ -29,56 +43,10 @@
                             </div>
                         </div>
 
-                        <!-- Reciever Message-->
-                        <div class="media w-50 ml-auto mb-3">
-                            <div class="media-body">
-                                <div class="bg-primary rounded py-2 px-3 mb-2">
-                                    <p class="text-small mb-0 text-white">Test which is a new approach to have all solutions</p>
-                                </div>
-                                <p class="small text-muted">12:00 PM | Aug 13</p>
-                            </div>
-                        </div>
-
-                        <!-- Sender Message-->
-                        <div class="media w-50 mb-3"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50" class="rounded-circle">
-                            <div class="media-body ml-3">
-                                <div class="bg-light rounded py-2 px-3 mb-2">
-                                    <p class="text-small mb-0 text-muted">Test, which is a new approach to have</p>
-                                </div>
-                                <p class="small text-muted">12:00 PM | Aug 13</p>
-                            </div>
-                        </div>
-
-                        <!-- Reciever Message-->
-                        <div class="media w-50 ml-auto mb-3">
-                            <div class="media-body">
-                                <div class="bg-primary rounded py-2 px-3 mb-2">
-                                    <p class="text-small mb-0 text-white">Apollo University, Delhi, India Test</p>
-                                </div>
-                                <p class="small text-muted">12:00 PM | Aug 13</p>
-                            </div>
-                        </div>
-
-                        <!-- Sender Message-->
-                        <div class="media w-50 mb-3"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50" class="rounded-circle">
-                            <div class="media-body ml-3">
-                                <div class="bg-light rounded py-2 px-3 mb-2">
-                                    <p class="text-small mb-0 text-muted">Test, which is a new approach</p>
-                                </div>
-                                <p class="small text-muted">12:00 PM | Aug 13</p>
-                            </div>
-                        </div>
-
-                        <!-- Reciever Message-->
-                        <div class="media w-50 ml-auto mb-3">
-                            <div class="media-body">
-                                <div class="bg-primary rounded py-2 px-3 mb-2">
-                                    <p class="text-small mb-0 text-white">Apollo University, Delhi, India Test</p>
-                                </div>
-                                <p class="small text-muted">12:00 PM | Aug 13</p>
-                            </div>
-                        </div>
-
+                        @endif
+                        @empty
+                        <div>No hay mensajes</div>
+                     @endforelse
                     </div>
 
 
