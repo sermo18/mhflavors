891c28ffa897b5dc02c740154cd9c38c04ee4496
sergio <sermo14.sm@gmail.com>
2021-06-08 19:58:12 +0200

    primer commit local

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -46,7 +46,7 @@
                     <h5 class="card-text p-3">Marca: {{ $sabor->marca }}</h5>
                     @if(!$sabor->valoracion)
                     <h5 class="card-text p-3">Valoración: 0 / 5</h5>
-                    @elseif(is_int($sabor->valoracion / $sabor->votos))
+                    @elseif(is_int($sabor->valoracion / $sabor->votos) == bool(true))
                     <h5 class="card-text p-3">Valoración: {{$sabor->valoracion / $sabor->votos}} / 5 </h5>
                     @else
                     <h5 class="card-text p-3">Valoración: {{ number_format($sabor->valoracion / $sabor->votos, 2, '.', '')}} / 5 </h5>
